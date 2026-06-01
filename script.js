4cc7b9516c2c92680d7b73c1536875282336bd45
Dulni115 <dulnibinanthi@gmail.com>
2026-06-01 21:03:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const hero = document.querySelector(".hero");
+
+const images = [
+    "images/1.jpg.jpeg",
+    "images/02.jpg.jpeg",
+    "images/03.jpg.jpeg",
+    "images/04.jpg.jpeg",
+    "images/05.jpg.jpeg"
+];
+
+let current = 0;
+
+function changeSlide() {
+
+    hero.style.backgroundImage =
+        `url('${images[current]}')`;
+
+    current++;
+
+    if(current >= images.length){
+        current = 0;
+    }
+}
+
+changeSlide();
+
+setInterval(changeSlide, 3500);
