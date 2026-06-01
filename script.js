7fab202e591d2df3d0b855e77ab5aa433f1d61b4
Dulni115 <dulnibinanthi@gmail.com>
2026-06-01 21:33:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hero = document.querySelector(".hero");
 
-// Images folder එකේ නම් නිවැරදිව මෙලෙසම තිබිය යුතුය.
+
 const images = [
     "images/1.jpg.jpeg",
     "images/02.jpg.jpeg",
@@ -12,7 +12,7 @@ const images = [
 let current = 0;
 
 function changeSlide() {
-    // බැක්ටික් (``) භාවිතා කර ඇති නිසා code එක හරියටම ක්‍රියා කරයි
+   
     hero.style.backgroundImage = `url('${images[current]}')`;
 
     current++;
@@ -22,8 +22,6 @@ function changeSlide() {
     }
 }
 
-// මුලින්ම පලවෙනි photo එක load වීමට
 changeSlide();
 
-// සෑම තත්පර 3.5 කට වරක් පසුබිම් පින්තූරය මාරු වේ
 setInterval(changeSlide, 3500);
