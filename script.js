d7f8ea849d2bd5ddf470227a838dcaf5fbc97eed
Dulni115 <dulnibinanthi@gmail.com>
2026-06-01 21:50:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const hero = document.querySelector(".hero");
 
+// GitHub Pages වල පින්තූර හරියටම load වෙන්න './images/' ලෙස වෙනස් කලා
 const images = [
-    "images/1.jpg.jpeg",
-    "images/02.jpg.jpeg",
-    "images/03.jpg.jpeg",
-    "images/04.jpg.jpeg",
-    "images/05.jpg.jpeg"
+    "./images/1.jpg.jpeg",
+    "./images/02.jpg.jpeg",
+    "./images/03.jpg.jpeg",
+    "./images/04.jpg.jpeg",
+    "./images/05.jpg.jpeg"
 ];
 
 let current = 0;
@@ -20,6 +21,8 @@ function changeSlide() {
     }
 }
 
+// මුලින්ම පලවෙනි photo එක පෙන්වීමට
 changeSlide();
 
+// තත්පර 3කට වරක් ස්ලයිඩ් වීම
 setInterval(changeSlide, 3000);
